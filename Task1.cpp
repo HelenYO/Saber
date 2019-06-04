51d20657b9011fefe38b9c8dc7db3ba201766208
Lena Ilina <ferr.98@mail.ru>
2019-06-04 21:01:23 +0300

    ...

--- Task1.cpp
+++ Task1.cpp
@@ -16,18 +16,24 @@ struct Digit {
 
     std::string to_string() {
         std::string ans(data.size(), '0');
-        for (int i = 0; i < data.size(); ++i) {
+        for (size_t i = 0; i < data.size(); ++i) {
             ans[i] = data[i] ? '1' : '0';
         }
         return ans;
     }
 
-public :
+    friend std::ostream& operator << (std::ostream &out, Digit &digit) {
+        out << digit.to_string();
+        return out;
+    }
+
+
+private:
     std::vector<bool> data;
 };
 
 int main() {
     int a = -5;
     Digit b(a);
-    std::cout << b.to_string();
+    std::cout << b;
 }
